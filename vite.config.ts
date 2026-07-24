de55d156edeb5a6f89fc30a9e6ef15c2fa2fe825
Birattam Rajak <birattamrajak.efrac@gmail.com>
2026-07-24 14:44:31 +0530

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), 
+    tailwindcss(),
+    ],
+  server: {
+    allowedHosts: [
+      'spotty-dogs-worry.loca.lt',
+    ],
+    host: true,
+    port:5179
+  }
 })
